f4d7e99ecb774fed9b1b742e7c1c2d30496c6e1e
markiian-h <markiian.hashchuk@gmail.com>
2021-12-01 01:54:15 +0200

    Settings changes

--- car_shop/settings.py
+++ car_shop/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+
+import django_heroku
 import environ
 import dj_database_url
 
@@ -161,3 +163,5 @@ AUTHENTICATION_BACKENDS = (
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+django_heroku.settings(locals())
