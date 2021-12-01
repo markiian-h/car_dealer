c1388ecd80b035694a2753fa28f6d4ea5a60d861
markiian-h <markiian.hashchuk@gmail.com>
2021-12-01 18:11:49 +0200

    Add settings for AWS S3

--- car_shop/settings.py
+++ car_shop/settings.py
@@ -165,3 +165,10 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 django_heroku.settings(locals())
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_ACCESS_KEY_ID = env('AWS_S3_ACCESS_KEY_ID')
+AWS_S3_SECRET_ACCESS_KEY = env('AWS_S3_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_QUERYSTRING_AUTH = False
